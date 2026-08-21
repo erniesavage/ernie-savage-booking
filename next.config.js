d852acc588080a7aea892630b1456fc633649c7e
Ernie Savage <erniesavage@macbook-pro.myfiosgateway.com>
2026-08-21 01:51:19 -0400

    Redirect celebrate-nilsson and legacy paths to homepage

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
+  async redirects() {
+    return [
+      { source: '/celebrate-nilsson', destination: '/', permanent: true },
+      { source: '/nilsson', destination: '/', permanent: true },
+      { source: '/salon-concerts', destination: '/', permanent: true },
+    ];
   },
 };
 
