c803a8d88f206486c9e0da1f1b66991a58cc1c5b
Ernie Savage <erniesavage@macbook-pro.myfiosgateway.com>
2026-09-10 22:46:25 -0400

    Fix config syntax, redirects live

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} /
 const nextConfig = {
 async redirects() {
 return [
 { source: '/celebrate-nilsson', destination: '/', permanent: true },
 { source: '/nilsson', destination: '/', permanent: true },
 { source: '/salon-concerts', destination: '/', permanent: true },
-{ source: '/experience/:slug', destination: '/', permanent: true },
+{ source: '/experience/:slug*', destination: '/', permanent: true },
 { source: '/experiences', destination: '/', permanent: true },
 { source: '/privacy', destination: '/', permanent: true },
 { source: '/terms', destination: '/', permanent: true },
